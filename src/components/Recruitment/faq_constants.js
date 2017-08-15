ed3e5ad79f7a46cd594137c8da93ed285bc89081
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-08-15 09:56:58 -0700

    creating faq page, perhaps own reusable component

---
+++ src/components/Recruitment/faq_constants.js
@@ -0,0 +1,67 @@
+const faq_constants = [
+  {
+    question: 'What makes PSE different from other organizations?',
+    answer:
+      "PSE differentiates itself from other business fraternities and organizations both in its learning philosophy and specialization. We prepare our brothers with the knowledge and skills necessary to succeed in today's business world, but go further to provide opportunities to utilize and hone these skills within projects across various industries. PSE hosts networking opportunities and corporate infosessions with professionals and alumni, and puts its members in positions to become their best possible professional selves. But PSE provides far more than professional development - as a fraternity, we are an intimate, close-knit environment where brothers can build strong personal relationships, and lasting friendships."
+  },
+  {
+    question: 'Why should I join PSE?',
+    answer: ''
+  },
+  {
+    question: 'What do you look for in recruits?',
+    answer: ''
+  },
+  {
+    question: 'Are all recruitment events mandatory?',
+    answer: ''
+  },
+  {
+    question: 'Is there a GPA minimum?',
+    answer: ''
+  },
+  {
+    question: 'Is there an age/year ceiling?',
+    answer: ''
+  },
+  {
+    question: "I'm not an ____ major. Can I still join?",
+    answer: ''
+  },
+  {
+    question: 'How many PMs does PSE accept each semester?',
+    answer: ''
+  },
+  {
+    question: 'Why do you not use terms such as "rush" or "pledge"?',
+    answer: ''
+  },
+  {
+    question: 'What is the time commitment for a prospective member?',
+    answer: ''
+  },
+  {
+    question: 'Will I be hazed as a prospective member?',
+    answer: ''
+  },
+  {
+    question: '',
+    answer: ''
+  },
+  {
+    question: '',
+    answer: ''
+  },
+  {
+    question: '',
+    answer: ''
+  },
+  {
+    question: '',
+    answer: ''
+  },
+  {
+    question: '',
+    answer: ''
+  }
+];
