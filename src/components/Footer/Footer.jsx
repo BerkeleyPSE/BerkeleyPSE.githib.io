3a4134da1f209a431d0930fdb2b0ff53a27c430b
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-08-17 00:19:36 -0700

    mapped over link items in Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,8 +8,41 @@ import { Link } from 'react-router-dom';
 // Local Helper Files & Components
 import { animations } from '../../stylesheets/Animations.js';
 
+const mediaLinkItems = [
+  {
+    media: 'facebook',
+    link: 'https://www.facebook.com/berkeleypse',
+    icon: 'fa-facebook-official'
+  },
+  {
+    media: 'instagram',
+    link: 'https://www.instagram.com/berkeleypse',
+    icon: 'fa-instagram'
+  },
+  {
+    media: 'twitter',
+    link: 'https://www.twitter.com/berkeleypse',
+    icon: 'fa-twitter'
+  }
+];
+
 export default class Footer extends React.Component {
   render() {
+    const mediaLinks = mediaLinkItems.map(item => {
+      return (
+        <Link
+          className={css(styles.link, styles.mediaLink)}
+          to={item.link}
+          target="blank"
+        >
+          <i
+            className={css(styles.icon) + ` fa ${item.icon}`}
+            aria-hidden="true"
+          />
+        </Link>
+      );
+    });
+
     return (
       <div className={css(styles.footerContainer, animations.fadeInUp)}>
         <div className={css(styles.copyrightContainer)}>
@@ -18,36 +51,7 @@ export default class Footer extends React.Component {
           </p>
         </div>
         <div className={css(styles.mediaContainer)}>
-          <Link
-            className={css(styles.link, styles.mediaLink)}
-            to={'https://www.facebook.com/berkeleypse'}
-            target="blank"
-          >
-            <i
-              className={css(styles.icon) + ' fa fa-facebook-official'}
-              aria-hidden="true"
-            />
-          </Link>
-          <Link
-            className={css(styles.link, styles.mediaLink)}
-            to={'https://www.instagram.com/berkeleypse'}
-            target="blank"
-          >
-            <i
-              className={css(styles.icon) + ' fa fa-instagram'}
-              aria-hidden="true"
-            />
-          </Link>
-          <Link
-            className={css(styles.link, styles.mediaLink)}
-            to={'https://www.twitter.com/berkeleypse'}
-            target="blank"
-          >
-            <i
-              className={css(styles.icon) + ' fa fa-twitter'}
-              aria-hidden="true"
-            />
-          </Link>
+          {mediaLinks}
         </div>
         <div className={css(styles.creditContainer)}>
           <p className={css(styles.credit, styles.text)}>
@@ -86,11 +90,15 @@ const styles = StyleSheet.create({
   },
 
   copyrightContainer: {
-    padding: '0 10px'
+    padding: '5px 10px'
+  },
+
+  mediaContainer: {
+    padding: '5px 0'
   },
 
   creditContainer: {
-    padding: '0 10px'
+    padding: '5px 10px'
   },
 
   link: {
