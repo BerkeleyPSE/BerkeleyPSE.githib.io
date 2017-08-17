5a535a6174b5b4df5add27b06a4af030d00aac24
Rahul Rangnekar <rrangnekar96@gmail.com>
2017-08-16 21:51:00 -0700

    added home info

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,42 @@
 // import React
 import React from 'react';
 
-// import inline stylesheets
+// NPM Modules
 import { StyleSheet, css } from 'aphrodite';
+import { Carousel } from 'react-responsive-carousel';
+
+const homeInfo = [
+  {
+    imgSrc: '/images/home/brotherhood.jpg',
+    message: 'Join Our Brotherhood',
+    buttonText: 'Apply',
+    buttonLink: '/app'
+  },
+  {
+    imgSrc: '/images/home/funny.JPG',
+    message: 'Hahaha',
+    buttonText: 'This is us',
+    buttonLink: 'Gotta laugh'
+  }
+];
 
 export default class Home extends React.Component {
   render() {
-    return <div className={css(styles.homeContainer)} />;
+    const carouselItems = homeInfo.map((item, index) => {
+      return (
+        <div>
+          <img src={item.imgSrc} />
+        </div>
+      );
+    });
+
+    return (
+      <div className={css(styles.homeContainer)}>
+        <Carousel>
+          {carouselItems}
+        </Carousel>
+      </div>
+    );
   }
 }
 
