fcc41ee2cdcc388a2dbc810cf37ebf5f708325dc
Rahul Rangnekar <rrangnekar96@gmail.com>
2018-03-24 20:02:10 -0700

    trying this

--- client/src/containers/Brothers/Executives.jsx
+++ client/src/containers/Brothers/Executives.jsx
@@ -28,7 +28,7 @@ class Executives extends Component {
     const president = executives.filter(
       bro => bro.position.value === "president"
     )[0];
-    const presIndex = executives.indexOf(president.key);
+    const vps = executives.filter(bro => bro.position.value !== "president");
     const vps = [
       ...executives.slice(0, presIndex),
       ...executives.slice(presIndex + 1)
